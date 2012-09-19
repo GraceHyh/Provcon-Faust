41ebaf3519b9cc53672392a5d1db9ca749ddfe5e
Martin Baulig <martin.baulig@xamarin.com>
2012-09-19 04:49:41 +0200

    Add self-hosting service.

---
+++ TestWCF/Service/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace TestWCF.Service
+{
+	class Service
+	{
+		static void Main()
+		{
+			var host = new ServiceHost(typeof(MyService));
+			host.Open();
+			Console.WriteLine("Service running");
+			foreach (var se in host.Description.Endpoints)
+				Console.WriteLine(se.Address);
+			Console.ReadLine();
+			host.Close();
+		}
+	}
+}
