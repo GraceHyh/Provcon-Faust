83fbc02d2c5a3d63445c4decb921675808d8fc5a
Martin Baulig <martin.baulig@xamarin.com>
2012-11-04 23:22:51 +0100

    Rename TestProxyAuth -> TestAuthentication.

--- TestAuthentication/Main.cs
+++ TestAuthentication/Main.cs
@@ -30,7 +30,7 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using Mono.Security.Protocol.Ntlm;
 
-namespace ProvconFaust.TestProxyAuth
+namespace ProvconFaust.TestAuthentication
 {
 	class MainClass
 	{
