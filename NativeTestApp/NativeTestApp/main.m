5dd4d76199afd503b7aad06f99c9ba0aac37c8c2
Martin Baulig <martin.baulig@xamarin.com>
2012-11-15 12:38:23 +0100

    Testing JPEG compression in Objective C.

---
+++ NativeTestApp/NativeTestApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NativeTestApp
+//
+//  Created by Martin Baulig on 11/15/12.
+//  Copyright (c) 2012 Xamarin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
