327a18f62d0900fe40213ba1188eb079cdc39a76
Martin Baulig <martin.baulig@xamarin.com>
2012-11-19 17:09:42 +0100

    Playing around with WSDL import.

---
+++ WsdlImport/Test.cs
@@ -0,0 +1,172 @@
+//
+// Test.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2012 Xamarin Inc. (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Xml;
+using System.Text;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.SyntaxHelpers;
+
+using WS = System.Web.Services.Description;
+
+namespace WsdlImport {
+
+	[TestFixture]
+	public class Test {
+
+		const string WspNamespace = "http://schemas.xmlsoap.org/ws/2004/09/policy";
+
+		[Test]
+		public void BasicHttpBinding ()
+		{
+			var doc = Utils.GetBasicHttpMetadata ();
+			BasicHttpBinding (doc);
+		}
+
+		void CheckSoapBinding (object extension, string label)
+		{
+			Assert.That (extension, Is.InstanceOfType (typeof (WS.SoapBinding)), label);
+			var soap = (WS.SoapBinding)extension;
+			Assert.That (soap.Style, Is.EqualTo (WS.SoapBindingStyle.Document), label + "a");
+			Assert.That (soap.Transport, Is.EqualTo (WS.SoapBinding.HttpTransport), label + "b");
+			Assert.That (soap.Required, Is.False, label + "c");
+		}
+
+		void CheckBasicHttpBinding (Binding binding, string scheme, BasicHttpSecurityMode security, string label)
+		{
+			Assert.That (binding, Is.InstanceOfType (typeof (BasicHttpBinding)), label);
+			var basicHttp = (BasicHttpBinding)binding;
+			Assert.That (basicHttp.EnvelopeVersion, Is.EqualTo (EnvelopeVersion.Soap11), label + "a");
+			Assert.That (basicHttp.MessageVersion, Is.EqualTo (MessageVersion.Soap11), label + "b");
+			Assert.That (basicHttp.Scheme, Is.EqualTo (scheme), label + "c");
+			Assert.That (basicHttp.TransferMode, Is.EqualTo (TransferMode.Buffered), label + "d");
+			Assert.That (basicHttp.MessageEncoding, Is.EqualTo (WSMessageEncoding.Text), label + "e");
+			Assert.That (basicHttp.Security, Is.Not.Null, label + "f");
+			Assert.That (basicHttp.Security.Mode, Is.EqualTo (security), label + "g");
+
+			var elements = basicHttp.CreateBindingElements ();
+			Assert.That (elements, Is.Not.Null, label + "h");
+			Assert.That (elements.Count, Is.EqualTo (2), label + "i");
+			
+			TextMessageEncodingBindingElement textElement = null;
+			HttpTransportBindingElement transportElement = null;
+			
+			foreach (var element in elements) {
+				if (element is TextMessageEncodingBindingElement)
+					textElement = (TextMessageEncodingBindingElement)element;
+				else if (element is HttpTransportBindingElement)
+					transportElement = (HttpTransportBindingElement)element;
+				else
+					Assert.Fail (label + "j");
+			}
+			
+			Assert.That (textElement, Is.Not.Null, label + "k");
+			Assert.That (transportElement, Is.Not.Null, label + "l");
+			
+			Assert.That (textElement.WriteEncoding, Is.InstanceOfType (typeof(UTF8Encoding)), label + "m");
+			Assert.That (transportElement.Realm, Is.Empty, label + "n");
+			Assert.That (transportElement.Scheme, Is.EqualTo (scheme), label + "o");
+			Assert.That (transportElement.TransferMode, Is.EqualTo (TransferMode.Buffered), label + "p");
+		}
+
+		public void BasicHttpBinding (MetadataSet doc)
+		{
+			var sd = (WS.ServiceDescription)doc.MetadataSections [0].Metadata;
+			Assert.That (sd.Bindings.Count, Is.EqualTo (1), "#1");
+
+			var binding = sd.Bindings [0];
+			Assert.That (binding.ExtensibleAttributes, Is.Null, "#2a");
+			Assert.That (binding.Extensions, Is.Not.Null, "#2b");
+			Assert.That (binding.Extensions.Count, Is.EqualTo (1), "#2c");
+
+			CheckSoapBinding (binding.Extensions [0], "#3");
+
+			var importer = new WsdlImporter (doc);
+
+			var bindings = importer.ImportAllBindings ();
+			Assert.That (bindings, Is.Not.Null, "#4a");
+			Assert.That (bindings.Count, Is.EqualTo (1), "#4b");
+
+			CheckBasicHttpBinding (bindings [0], "http", BasicHttpSecurityMode.None, "#5");
+		}
+
+		[Test]
+		public void BasicHttpsBinding ()
+		{
+			var doc = Utils.GetBasicHttpsMetadata ();
+			BasicHttpsBinding (doc);
+		}
+		
+		public void BasicHttpsBinding (MetadataSet doc)
+		{
+			var sd = (WS.ServiceDescription)doc.MetadataSections [0].Metadata;
+
+			Assert.That (sd.Extensions, Is.Not.Null, "#1");
+			Assert.That (sd.Extensions.Count, Is.EqualTo (1), "#1a");
+			Assert.That (sd.Extensions [0], Is.InstanceOfType (typeof (XmlElement)), "#1b");
+
+			var extension = (XmlElement)sd.Extensions [0];
+			Assert.That (extension.NamespaceURI, Is.EqualTo (WspNamespace), "#1c");
+			Assert.That (extension.LocalName, Is.EqualTo ("Policy"), "#1d");
+
+			Assert.That (sd.Bindings.Count, Is.EqualTo (1), "#2");
+			var binding = sd.Bindings [0];
+			Assert.That (binding.ExtensibleAttributes, Is.Null, "#2a");
+			Assert.That (binding.Extensions, Is.Not.Null, "#2b");
+			Assert.That (binding.Extensions.Count, Is.EqualTo (2), "#2c");
+
+			WS.SoapBinding soap = null;
+			XmlElement xml = null;
+
+			foreach (var ext in binding.Extensions) {
+				if (ext is WS.SoapBinding)
+					soap = (WS.SoapBinding)ext;
+				else if (ext is XmlElement)
+					xml = (XmlElement)ext;
+			}
+
+			CheckSoapBinding (soap, "#3");
+
+			var importer = new WsdlImporter (doc);
+			
+			var bindings = importer.ImportAllBindings ();
+			Assert.That (bindings, Is.Not.Null, "#4a");
+			Assert.That (bindings.Count, Is.EqualTo (1), "#4b");
+
+			CheckBasicHttpBinding (bindings [0], "https", BasicHttpSecurityMode.Transport, "#5");
+
+			Assert.That (xml, Is.Not.Null, "#6");
+
+			Assert.That (xml.NamespaceURI, Is.EqualTo (WspNamespace), "#6a");
+			Assert.That (xml.LocalName, Is.EqualTo ("PolicyReference"), "#6b");
+		}
+	}
+}
---
+++ WsdlImport/Utils.cs
@@ -0,0 +1,124 @@
+//
+// Utils.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2012 Xamarin Inc. (http://www.xamarin.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.IO;
+using System.Xml;
+using System.Reflection;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using WS = System.Web.Services.Description;
+
+namespace WsdlImport {
+
+	public static class Utils {
+
+		const string HttpUri = "http://tempuri.org/TestHttp/";
+		const string HttpsUri = "https://tempuri.org/TestHttps/";
+
+		public static MetadataSet GetBasicHttpMetadata ()
+		{
+			var exporter = new WsdlExporter ();
+			
+			var cd = new ContractDescription ("MyContract");
+			
+			exporter.ExportEndpoint (new ServiceEndpoint (
+				cd, new BasicHttpBinding (), new EndpointAddress (HttpUri)));
+			
+			var doc = exporter.GetGeneratedMetadata ();
+			return doc;
+		}
+		
+		public static MetadataSet GetBasicHttpsMetadata ()
+		{
+			var exporter = new WsdlExporter ();
+			
+			var cd = new ContractDescription ("MyContract");
+			
+			exporter.ExportEndpoint (new ServiceEndpoint (
+				cd, new BasicHttpsBinding (), new EndpointAddress (HttpsUri)));
+			
+			var doc = exporter.GetGeneratedMetadata ();
+			return doc;
+		}
+		
+		public static void Save (string filename, WS.ServiceDescription service)
+		{
+			using (var file = new StreamWriter (filename, false)) {
+				var writer = new XmlTextWriter (file);
+				writer.Formatting = Formatting.Indented;
+				service.Write (writer);
+			}
+		}
+
+		public static void Save (string filename, MetadataSet metadata)
+		{
+			using (var file = new StreamWriter (filename, false)) {
+				var writer = new XmlTextWriter (file);
+				writer.Formatting = Formatting.Indented;
+				metadata.WriteTo (writer);
+			}
+		}
+
+		public static MetadataSet Load (string filename)
+		{
+			using (var file = new StreamReader (filename)) {
+				var reader = new XmlTextReader (file);
+				return MetadataSet.ReadFrom (reader);
+			}
+		}
+
+		public static MetadataSet LoadFromResource (string name)
+		{
+			var asm = Assembly.GetExecutingAssembly ();
+			using (var stream = asm.GetManifestResourceStream (name)) {
+				var reader = new XmlTextReader (stream);
+				return MetadataSet.ReadFrom (reader);
+			}
+		}
+
+		public static MetadataSet LoadBasicHttpMetadata ()
+		{
+			return LoadFromResource ("http.xml");
+		}
+
+		public static MetadataSet LoadBasicHttpsMetadata ()
+		{
+			return LoadFromResource ("https.xml");
+		}
+
+		internal static void SaveMetadata ()
+		{
+			var basicHttp = GetBasicHttpMetadata ();
+			Utils.Save ("http.xml", basicHttp);
+			
+			var basicHttps = GetBasicHttpsMetadata ();
+			Utils.Save ("https.xml", basicHttps);
+			
+			Console.WriteLine ("Metadata saved.");
+		}
+	}
+}
